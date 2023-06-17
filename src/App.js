19fc281be1ef74c4da7ee153f0ab957a648fe699
gogi999 <g.petrovic9@gmail.com>
2023-06-17 12:50:57 +0200

    Added basic setup and started building components

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+import Button from './components/Button';
+import FormAddFriend from './components/FormAddFriend';
+import FormSplitBill from './components/FormSplitBill';
+import FriendsList from './components/FriendsList';
+import { initialFriends } from './initialFriends';
+
+const App = () => {
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  const handleShowAddFriend = () => {
+    setShowAddFriend((show) => !show);
+  }
+
+  const handleAddFriend = (friend) => {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
+  const handleSelection = (friend) => {
+    // setSelectedFriend(friend);
+    setSelectedFriend((currSelected) => 
+      currSelected?.id === friend.id ? null : friend
+    );
+    setShowAddFriend(false);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList 
+          friends={friends} 
+          onSelection={handleSelection} 
+          selectedFriend={selectedFriend}  
+        />
+        {showAddFriend && (
+          <FormAddFriend 
+            onAddFriend={handleAddFriend}
+          />
+        )}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+      {selectedFriend && (
+        <FormSplitBill selectedFriend={selectedFriend} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormSplitBill.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Button from './Button';
+
+const FormSplitBill = ({ selectedFriend }) => {
+    return (
+        <form className="form-split-bill">
+            <h2>Split a bill with {selectedFriend.name}</h2>
+            <label>💰 Bill value</label>
+            <input
+                type="text"
+            />
+
+            <label>🧍‍♀️ Your expense</label>
+            <input
+                type="text"
+            />
+
+            <label>👫 {selectedFriend.name}'s expense</label>
+            <input type="text" disabled />
+
+            <label>🤑 Who is paying the bill</label>
+            <select
+                value="user"
+                onChange={() => {}}
+            >
+                <option value="user">You</option>
+                <option value="friend">{selectedFriend.name}</option>
+            </select>
+
+            <Button>Split Bill</Button>
+        </form>
+    );
+}
+
+export default FormSplitBill;
