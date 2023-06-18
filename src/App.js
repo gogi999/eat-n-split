8da6f0ed4be1173f2cbffb3258e0dfa5db4cb418
gogi999 <g.petrovic9@gmail.com>
2023-06-18 11:21:36 +0200

    eat-n-split v1 completed

--- src/App.js
+++ src/App.js
@@ -28,6 +28,17 @@ const App = () => {
     setShowAddFriend(false);
   }
 
+  const handleSplitBill = (value) => {
+    setFriends((friends) => friends.map((friend) => 
+      friend.id === selectedFriend.id 
+        ? { ...friend, balance: friend.balance + value}
+        : friend
+      )
+    );
+
+    setSelectedFriend(null);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -46,7 +57,10 @@ const App = () => {
         </Button>
       </div>
       {selectedFriend && (
-        <FormSplitBill selectedFriend={selectedFriend} />
+        <FormSplitBill 
+          selectedFriend={selectedFriend} 
+          onSplitBill={handleSplitBill}   
+        />
       )}
     </div>
   );
